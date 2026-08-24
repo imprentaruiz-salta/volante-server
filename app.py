174db9f216cfdc194e82fda53e21af9c7b5c03ef
imprentaruiz-salta <impresiones.ruiz.sla@gmail.com>
2026-08-24 07:57:15 -0700

    Cambiar mascota por nueva imagen

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def index():
 def imprenta_ruiz():
     """Página pública de Imprenta Ruiz, con preview al compartir el enlace."""
     html = render_template("ruiz.html")
-    animated_mascot = '<img class="mascota-float mascota-animada" style="width:125px;max-height:180px" src="https://share.zapia.com/364edisw4oof6c0bxul0tz" alt="Mascota animada de Imprenta Ruiz">'
+    animated_mascot = '<img class="mascota-float mascota-animada" style="width:125px;max-height:180px" src="https://share.zapia.com/1lff1gcc8yb0dyigx10bja" alt="Mascota animada de Imprenta Ruiz">'
     mascot_start = html.find('<img class="mascota-float"')
     if mascot_start >= 0:
         mascot_end = html.find('>', mascot_start)
